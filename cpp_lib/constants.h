8f8f14d07c5eae443caae7b810e8d1d5a3ae767a
Keith Gould <keithmgould@gmail.com>
2018-05-07 15:03:21 -0400

    use constexpr instead of define in constants.h

--- cpp_lib/constants.h
+++ cpp_lib/constants.h
@@ -1,54 +1,54 @@
 #ifndef __BEAKER_CONSTANTS__
 #define __BEAKER_CONSTANTS__
 
-// These constants have to do with Beaker's
+// These constexprants have to do with Beaker's
 // physical characteristics, or related computations
-#define PI_OVER_ONE_EIGHTY 0.017453292519943 // for degrees to radians
-#define WHEEL_RADIUS .042 // in meters
-#define WHEEL_CIRCUMFERECE 2 * PI * WHEEL_RADIUS
-#define FULL_ROTATION_EDGE_EVENTS 600 // 18.75 * 32
-#define CLICKS_TO_RADIANS 2 * PI / FULL_ROTATION_EDGE_EVENTS
-#define THETA_OFFSET 0.01 // due to minorly non-level IMU
+constexpr float PI_OVER_ONE_EIGHTY = 0.017453292519943; // for degrees to radians
+constexpr float WHEEL_RADIUS = .042; // in meters
+constexpr int FULL_ROTATION_EDGE_EVENTS = 600; // 18.75 * 32
+constexpr float THETA_OFFSET = 0.01; // due to minorly non-level IMU
+constexpr float WHEEL_CIRCUMFERECE = 2 * PI * WHEEL_RADIUS;
+constexpr float CLICKS_TO_RADIANS = 2 * PI / FULL_ROTATION_EDGE_EVENTS;
 
 // LED on side of robot
-#define INDICATOR 8
+constexpr int INDICATOR = 8;
 
 // pins for the motor encoders
-#define RH_ENCODER_A 3 // interupt pin
-#define RH_ENCODER_B 5
-#define LH_ENCODER_A 2 // interupt pin
-#define LH_ENCODER_B 4
+constexpr int RH_ENCODER_A = 3; // interupt pin
+constexpr int RH_ENCODER_B = 5;
+constexpr int LH_ENCODER_A = 2; // interupt pin
+constexpr int LH_ENCODER_B = 4;
 
 // pin for piezo buzzer
-#define BUZZER_PIN 9
+constexpr int BUZZER_PIN = 9;
 
 // pins for radio
-#define RADIO_CE 6
-#define RADIO_CSN 7
+constexpr int RADIO_CE = 6;
+constexpr int RADIO_CSN = 7;
 
 // pins for PWM Motor Control
-#define LEFT_MOTOR_DRIVER 10
-#define RIGHT_MOTOR_DRIVER 11
+constexpr int LEFT_MOTOR_DRIVER = 10;
+constexpr int RIGHT_MOTOR_DRIVER = 11;
 
 // Duration (in millisecs) for each loop.
 // MAKE SURE: that the position control is
 // 3-4 times slower than motor control
-#define MOTOR_CONTROL_TIMESTEP 5
-#define POSITION_CONTROL_TIMESTEP 20
+constexpr int MOTOR_CONTROL_TIMESTEP = 5;
+constexpr int POSITION_CONTROL_TIMESTEP = 20;
 
 // Minimum/Maximum values to send to motor drivers
 // Should ALWAYS be between -1 and 1.
-// To constrain to 50% power use -.5 and .5
-#define MINIMUM_GAIN -1
-#define MAXIMUM_GAIN 1
+// To constexprrain to 50% power use -.5 and .5
+constexpr int MINIMUM_GAIN = -1;
+constexpr int MAXIMUM_GAIN = 1;
 
 // default Motor PID values
-#define MOTOR_P_PARAM 0.015
-#define MOTOR_I_PARAM 0
-#define MOTOR_D_PARAM 0.05
+constexpr float MOTOR_P_PARAM = 0.015;
+constexpr float MOTOR_I_PARAM = 0;
+constexpr float MOTOR_D_PARAM = 0.05;
 
 // Used by motors for direction of rotation
-#define FORWARD 1
-#define BACKWARD -1
+constexpr int FORWARD = 1;
+constexpr int BACKWARD = -1;
 
 #endif
