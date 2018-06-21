e8fa953b0acccb5afd08a7b91d09299c6c9b1e86
Keith Gould <keithmgould@gmail.com>
2018-06-21 20:08:54 +0000

    sheesh

--- raspberry_pi/terminal/test_response_time.py
+++ raspberry_pi/terminal/test_response_time.py
@@ -11,29 +11,35 @@ class Agent:
     self.arduino = Arduino()
 
   def run(self):
-    self.ensureaStopped()
+    self.ensureStopped()
     issueTime = current_milli_time()
     self.arduino.writeMessage("W10")
 
     while True:
-      observation = self.arduino.writeMessageAndWait("S")
-      wheelSpeed = observation[3]
+      observation = self.arduino.writeMessageAndWait("S") # get State
+      print("obs: {}".format(observation[3]))
+      wheelSpeed = float(observation[3])
       if(wheelSpeed > 0.0):
         responseTime = current_milli_time() - issueTime
         print("response time: {}".format(responseTime))
         return
+      else:
+        print("{},{}".format(wheelSpeed, observation))
 
-    def ensureaStopped(self):
-      print("Ensuring stopped...")
-      while True:
-        observation = self.arduino.writeMessageAndWait("S") # get State
-        wheelSpeed = observation[3]
-        if(wheelSpeed == 0):
-          print("Stopped! :)")
-          return
-        else:
-          self.arduino.writeMessage("W0")
-          time.sleep(0.5)
+
+  def ensureStopped(self):
+    print("Ensuring stopped...")
+    while True:
+      observation = self.arduino.writeMessageAndWait("S") # get State
+      print(observation)
+      wheelSpeed = float(observation[3])
+      if(wheelSpeed == 0.0):
+        print("Stopped! :)")
+        return
+      else:
+        print("wheel speed is: {}".format(wheelSpeed))
+        self.arduino.writeMessage("W0")
+        time.sleep(0.5)
 
 agent = Agent()
 agent.run()
