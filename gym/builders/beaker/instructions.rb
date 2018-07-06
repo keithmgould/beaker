90cfce425e742e45f7cc9c72a452c6a514c810aa
Keith Gould <keithmgould@gmail.com>
2018-07-06 16:09:12 -0400

    finishes to the beaker URDF builder

--- gym/builders/beaker/instructions.rb
+++ gym/builders/beaker/instructions.rb
@@ -1,11 +1,11 @@
 require 'mustache'
 
 Mustache.template_path = File.dirname(__FILE__)
-Mustache.template_file = File.dirname(__FILE__) + '/template.mustache'
+Mustache.template_file = File.dirname(__FILE__) + '/main.mustache'
 view = Mustache.new
 
 #---------------------------------------------------
-# main body (base)
+# body (base)
 body_x = 0.1016
 body_y = 0.0254
 body_z = 0.6604
@@ -17,7 +17,7 @@ view[:body_dimensions] = "#{body_x} #{body_y} #{body_z}"
 view[:wheel_rad] = 0.042
 view[:wheel_height] = 0.01905
 view[:wheel_length] = 0.0254
-wheel_x_offset = 0.04
+wheel_x_offset = 0.09398
 
 #---------------------------------------------------
 # indicators
@@ -25,8 +25,10 @@ side = 0.01
 view[:indicator_dimensions] = "#{side} #{side} #{side}"
 view[:indicator_gear_offset] = 0.03
 
-view[:left_side] = { dir: "left", wheel_x_offset: -wheel_x_offset }
-view[:right_side] = { dir: "right", wheel_x_offset: wheel_x_offset }
+#---------------------------------------------------
+# side differentiators
+view[:left_side] = { dir: "left", wheel_x_offset: -wheel_x_offset, color: "red" }
+view[:right_side] = { dir: "right", wheel_x_offset: wheel_x_offset, color: "green" }
 
 path = File.dirname(__FILE__) + '/../../beaker.urdf'
 open(path, 'w') { |f| f.puts view.render }
