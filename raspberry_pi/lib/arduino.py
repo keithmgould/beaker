c2df8d8e179b8eac70c44e8654cb3f3ab5a459e6
Keith Gould <keithmgould@gmail.com>
2018-08-22 12:24:18 -0400

    update power in arduino.py

--- raspberry_pi/lib/arduino.py
+++ raspberry_pi/lib/arduino.py
@@ -8,6 +8,14 @@ class Arduino:
                   parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
                   bytesize=serial.EIGHTBITS, timeout=1)
 
+  def accelerateMotorPower(self, acc):
+    acc = self.__constrain(acc, -0.5, 0.5);
+    self.__writeMessage("A" + str(acc))
+
+  def updateMotorPower(self, newPower):
+    newPower = self.__constrain(newPower, -10, 10);
+    self.__writeMessage("W" + str(newPower))
+
   def writeMessage(self, message):
     self.__writeMessage(message)
 
