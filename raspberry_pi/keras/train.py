659727fb8a086e2705565aba4a0d6e45e5a09584
Keith Gould <keithmgould@gmail.com>
2018-09-03 00:39:51 -0400

    formatting

--- raspberry_pi/keras/train.py
+++ raspberry_pi/keras/train.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 import pandas
 from keras.models import Sequential
 from keras.layers import Dense
@@ -10,27 +11,37 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 import pdb
 
+
+if(len(sys.argv) != 2):
+  print("give me a log file as argument please!")
+  exit()
+
+logfile = sys.argv[1]
 # load dataset
-dataframe = pandas.read_csv("./pid_log-20180823-55k.txt", header=None)
+dataframe = pandas.read_csv(logfile, header=None)
 dataset = dataframe.values
+dataset_total_length = len(dataset)
+dataset_train_length = int(dataset_total_length * 0.8)
+
+print("total data points: {}".format(dataset_total_length))
+print("training data points: {}".format(dataset_train_length))
+
 np.random.shuffle(dataset)
 
 # define base model
 def baseline_model():
-	# create model
-	model = Sequential()
-	model.add(Dense(256, input_dim=4, kernel_initializer='normal', activation='relu'))
-	model.add(Dense(1, kernel_initializer='normal'))
-	# Compile model
-	model.compile(loss='mean_squared_error', optimizer='adam')
-	return model
+  # create model
+  model = Sequential()
+  model.add(Dense(256, input_dim=4, kernel_initializer='normal', activation='relu'))
+  model.add(Dense(1, kernel_initializer='normal'))
+  # Compile model
+  model.compile(loss='mean_squared_error', optimizer='adam')
+  return model
 
-
-
-train_data_x = dataset[:50000,0:4]
-train_data_y = dataset[:50000,6]
-validate_data_x = dataset[50001:55000,0:4]
-validate_data_y = dataset[50001:55000,6] 
+train_data_x = dataset[:dataset_train_length,0:4]
+train_data_y = dataset[:dataset_train_length,4]
+validate_data_x = dataset[(dataset_train_length + 1):dataset_total_length,0:4]
+validate_data_y = dataset[(dataset_train_length + 1):dataset_total_length,4] 
 
 
 # fit(x=None, y=None, batch_size=None, epochs=1, verbose=1, 
@@ -41,13 +52,13 @@ validate_data_y = dataset[50001:55000,6]
 
 model = baseline_model()
 model.fit(
-	train_data_x, 
-	train_data_y, 
-	batch_size=32, 
-	shuffle=True,
-	epochs=1000,
-	verbose=1, 
-	validation_data=(validate_data_x, validate_data_y))
+  train_data_x, 
+  train_data_y, 
+  batch_size=32, 
+  shuffle=True,
+  epochs=1000,
+  verbose=1, 
+  validation_data=(validate_data_x, validate_data_y))
 
 print("Saving model...")
 model.save('model_h_256_b_32_e_1000.h5')
