e55a3299493148af20e0844c2526f27b4c9247db
Keith Gould <keithmgould@gmail.com>
2018-09-04 13:56:30 -0400

    throw balls, yo.

--- gym/beakerEnv.py
+++ gym/beakerEnv.py
@@ -2,10 +2,13 @@ from pybullet_envs.scene_stadium import SinglePlayerStadiumScene
 from pybullet_envs.env_bases import MJCFBaseBulletEnv
 from beakerBot import BeakerBot
 import gym, gym.spaces, gym.utils, gym.utils.seeding
+import random, math
 import numpy as np
 import pybullet as p
 import os, sys
 import pdb
+
+THROWBALLS = True
 
 class BeakerCam:
 	def __init__(self, bullet_client):
@@ -33,12 +36,30 @@ class BeakerBotBulletEnv(MJCFBaseBulletEnv):
 
 		rr = MJCFBaseBulletEnv._reset(self)
 		self.beakerCam = BeakerCam(self._p)
+		
+		if(THROWBALLS and self.stateId < 0):
+			self.frames_since_ball_thrown = 0
+			mass = .05
+			visualShapeId = -1
+			sphereRadius = 0.05
+			useMaximalCoordinates = 0
+			position = [0.3,0.3,0.2]
+			colSphereId = self._p.createCollisionShape(p.GEOM_SPHERE,radius=sphereRadius)
+			self.sphereUid = p.createMultiBody(mass,colSphereId,visualShapeId,position,useMaximalCoordinates=useMaximalCoordinates)
+			self._throw_ball()
 
 		if (self.stateId<0):
 			self.stateId = self._p.saveState()
-		
+
 		return rr
 	
+	def _throw_ball(self):
+		random_angle = random.randint(0,360)
+		xPos = 1 * math.cos(random_angle)
+		yPos = 1 * math.sin(random_angle)
+		p.resetBasePositionAndOrientation(self.sphereUid, [xPos,yPos,0.2], [0,0,0,1])
+		p.resetBaseVelocity(self.sphereUid, [-xPos * 2,-yPos * 2,3])
+
 	def _step(self, a):
 		self.robot.apply_action(a)
 		self.scene.global_step()
@@ -46,6 +67,12 @@ class BeakerBotBulletEnv(MJCFBaseBulletEnv):
 		done = np.abs(self.robot.theta) > .2
 		self.HUD(state, a, done)
 		self.camera_adjust(self.robot.robot_body.current_position())
+		if(THROWBALLS):
+			self.frames_since_ball_thrown += 1
+			if(self.frames_since_ball_thrown > 100):
+				self.frames_since_ball_thrown = 0
+				self._throw_ball()
+
 		return state, 1.0, done, {}
 
 	def camera_adjust(self, pos):
